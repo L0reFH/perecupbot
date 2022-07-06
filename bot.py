84c8c3518f22b075f2026bf93944e2b67dcfc8ad
L0reFH <L0reFH@mail.ru>
2022-07-06 17:28:00 +0600

    отправка поста по группам + небольшие исправления

--- bot.py
+++ bot.py
@@ -1,8 +1,41 @@
 import json
+import time
 from telebot import TeleBot
 import vk
 import random
 import requests
+
+
+groups = [
+    -62441045,
+    -203102543,
+    -184648211,
+    -68829811,
+    -204517084,
+    -212824275,
+    -157615038,
+    -151598880,
+    -146665911,
+    -130150864,
+    -134416509,
+    -158969191,
+    -146655979,
+    -202176632,
+    -199963188,
+    -210879159,
+    -203748128,
+    -128118706,
+    -147002621,
+    -200896907,
+    -207857905,
+    -203303458,
+    -204514980,
+    -7241540,
+    -208243426,
+    -144614991,
+    -200357122,
+    -91335741,
+    -156878704]
 
 
 class AuthException(Exception):
@@ -11,13 +44,13 @@ class AuthException(Exception):
 token = "5437580922:AAHhmA0rl2qr_A-SY2Fi-mXYWeU7_0qwOYo"
 user_id = 5388239974 # мой id
 # user_id = 1770636752 # id заказчика
+
 def vk_auth():
     i = 0
     global password
     global phone
     global vk_session
-    global vk_api
-    global lp
+
     while True:
         try:
             phone = int(input("Введите номер телефона: "))
@@ -25,8 +58,11 @@ def vk_auth():
         except:
             continue
     password = input("Введите пароль от аккаунта вк: ")
-    
-    vk_session = vk.UserAPI(user_login=phone, user_password=password, scope="wall, photos, friends, groups", v="5.131")
+
+    vk_session = vk.UserAPI(user_login=phone, user_password=password, scope="wall, photos, friends, groups", v="5.131", client_id=8203325)
+    print("Бот успешно запущен!")
+    print("Иди клепай свои объявления, шизик")
+
 
 vk_auth()
 
@@ -57,42 +93,61 @@ def request_post_photo(message):
 
 def create_post(message):
     if message.photo:
-        tb.send_message(user_id, f"Запостить вот-так?\n{post_text}")
         photo_info = tb.get_file(message.photo[len(message.photo)-1].file_id)
         global downloaded_photo
         downloaded_photo = tb.download_file(photo_info.file_path)
-
-        tb.send_photo(chat_id=user_id,photo=message.photo[-1].file_id)
-
-        send = tb.send_message(user_id, "Если нужно переделать пост отправь 'Ред'")
-        tb.register_next_step_handler(send, edit_post)
-            
+        
+        tb.send_message(user_id, "Проверь пост перед отправкой")
+        tb.send_message(user_id, post_text)
+        tb.send_photo(user_id, message.photo[-1].file_id)
+        send = tb.send_message(user_id, "Если нужно переделать пост, пиши 'ред', если пост готов и его нужно запостить, пиши 'пост'")
+        tb.register_next_step_handler(send, send_post)
+        
     else: 
         send = tb.send_message(user_id, "Пришли фотографию подиков")
         tb.register_next_step_handler(send, create_post)
 
-def edit_post(message):
+def send_post(message):
     if message.text:
         if message.text.lower() == 'ред':
-            request_post_text
-        elif message.text.lower() == "пост":
-            vk_session = vk.UserAPI(user_login=phone, user_password=password, scope="wall, photos, friends, groups", v="5.131")
-            up_url = vk_session.photos.getWallUploadServer()["upload_url"]
+            send = tb.send_message(user_id, "Пришли текст поста(Описание и цена)")
+            tb.register_next_step_handler(send, request_post_photo)
 
-        with open("img_0.jpg", "wb") as file:
+        elif message.text.lower() == "пост":
+            attachments = []
+            vk_session = vk.UserAPI(user_login=phone, user_password=password, scope="wall, photos, friends, groups", v="5.131", client_id=8203325)
+            up_url = vk_session.photos.getWallUploadServer()["upload_url"]
+                
+            
+            with open("img_0.jpg", "wb") as file:
                 file.write(downloaded_photo)
                 file.close()
+            
+            # загрузка фото на сервер 
+            with open("img_0.jpg", "rb") as file:
+                resp = requests.post(f"{up_url}", files={"file": file})
+                print(resp.json())
+                saveWallPhoto = vk_session.photos.saveWallPhoto(server=resp.json()["server"], photo=resp.json()["photo"], hash=resp.json()["hash"])
+                attachments = []
+                attachments.append("photo{}_{}".format(saveWallPhoto[0]["owner_id"], saveWallPhoto[0]["id"]))
                 
-        with open("img_0.jpg", "rb") as file:
-            resp = requests.post(f"{up_url}", files={"file": file})
-            saveWallPhoto = vk_session.photos.saveWallPhoto(server=resp.json()["server"], photo=resp.json()["photo"], hash=resp.json()["hash"])
-            attachments = []
-            attachments.append("photo{}_{}".format(saveWallPhoto[0]["owner_id"], saveWallPhoto[0]["id"]))
-            vk_session.wall.post(attachments=attachments, message=post_text)
-        
-        tb.send_message(user_id, "Пост создан")
+
+                for group in groups:
+                    try: 
+                        vk_session.wall.post(message=post_text, owner_id=group, attachments=attachments)
+                        tb.send_message(user_id, f"Удачный пост в группу: https://vk.com/club{group*-1}")
+                        time.sleep(1)
+                        
+
+                    except Exception as e:
+                        print(e)
+                        tb.send_message(user_id, f"Неудачный пост в группу: https://vk.com/club{group*-1}")
+                        continue
+
+                tb.send_message(user_id, "Работа окончена")
+                return
     else:
-        send = tb.send_message(user_id, "Если нужно переделать пост отправь 'Ред'")
-        tb.register_next_step_handler(send, edit_post)
+        send = tb.send_message(user_id, "Если нужно переделать пост, пиши 'ред', если пост готов и его нужно запостить, пиши 'пост'")
+        tb.register_next_step_handler(send, send_post)
     
 tb.polling()
